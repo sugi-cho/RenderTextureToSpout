a9ecf39b6b4d893f79292c8450bcd64b7ac1dc51
sugi.cho <komenari@gmail.com>
2017-01-23 21:15:12 +0900

    clean up

---
+++ RenderTextureToSpout/Assets/RenderTextureToSpout/Scripts/RenderTextureBridge.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class TextureEvent : UnityEvent<Texture> { }
+
+public class RenderTextureBridge : MonoBehaviour
+{
+
+    RenderTexture output;
+    public TextureEvent onCreateOutput;
+
+    void OnRenderImage(RenderTexture s, RenderTexture d)
+    {
+        if (!IsValidOutput(s))
+            CreateOutput(s);
+
+        Graphics.CopyTexture(s, output);
+        Graphics.Blit(s, d);
+    }
+
+    bool IsValidOutput(RenderTexture s)
+    {
+        return output != null && s.width == output.width && s.height == output.height;
+    }
+
+    void CreateOutput(RenderTexture s)
+    {
+        if (output != null)
+            output.Release();
+        output = new RenderTexture(s.width, s.height, s.depth, s.format);
+        output.Create();
+        onCreateOutput.Invoke(output);
+    }
+}
---
+++ RenderTextureToSpout/Assets/RenderTextureToSpout/Scripts/RenderTextureSpoutSender.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+namespace Spout
+{
+    public class RenderTextureSpoutSender : MonoBehaviour
+    {
+        public enum TextureFormat { DXGI_FORMAT_R32G32B32A32_FLOAT = 2, DXGI_FORMAT_R10G10B10A2_UNORM = 24, DXGI_FORMAT_R8G8B8A8_UNORM = 28, DXGI_FORMAT_B8G8R8A8_UNORM = 87 }
+        public string sharingName = "UnitySender";
+        public TextureFormat textureFormat = TextureFormat.DXGI_FORMAT_R8G8B8A8_UNORM;
+        bool senderIsCreated;
+
+        public Texture texture;
+
+        private int _createAttempts = 5;
+        private int _attempts = 0;
+
+        bool textureSizeHasChanged
+        {
+            get
+            {
+                var b = !(texture.width == width && texture.height == height);
+                width = texture.width;
+                height = texture.height;
+                return b;
+            }
+        }
+        int width = 0;
+        int height = 0;
+
+        void OnDisable()
+        {
+            _CloseSender();
+        }
+
+        public void SetTexture(Texture tex)
+        {
+            texture = tex;
+        }
+
+        void Update()
+        {
+            if (texture == null) return;
+            if (textureSizeHasChanged) _CloseSender();
+            if (!senderIsCreated) _CreateSender(texture);
+            if (senderIsCreated) Spout.instance.UpdateSender(sharingName, texture);
+        }
+
+        private void _CreateSender(Texture texture)
+        {
+            if (texture == null) return;
+            if (!Spout.instance.enabled) return;
+
+            if (!senderIsCreated)
+            {
+                Debug.Log("Sender is not created, creating one");
+                senderIsCreated = Spout.instance.CreateSender(sharingName, texture, (int)textureFormat);
+            }
+
+            _attempts++;
+            if (_attempts > _createAttempts) Debug.LogWarning(String.Format("There are problems with creating the sender {0}. Please check your settings or restart Unity.", sharingName));
+
+            Spout.instance.OnSenderStopped -= OnSenderStoppedDelegate;
+            Spout.instance.OnSenderStopped += OnSenderStoppedDelegate;
+
+            Spout.instance.OnAllSendersStopped -= OnAllSendersStoppedDelegate;
+            Spout.instance.OnAllSendersStopped += OnAllSendersStoppedDelegate;
+
+            Spout.instance.OnEnabled -= _OnSpoutEnabled;
+            Spout.instance.OnEnabled += _OnSpoutEnabled;
+        }
+
+        private void _OnSpoutEnabled()
+        {
+            //Debug.Log("SpoutSender._OnSpoutEnabled");
+            if (enabled)
+            {
+                //force a reconnection
+                enabled = !enabled;
+                enabled = !enabled;
+            }
+        }
+
+        private void _CloseSender()
+        {
+            Debug.Log("SpoutSender._CloseSender:" + sharingName);
+            if (senderIsCreated) Spout.CloseSender(sharingName);
+            _CloseSenderCleanUpData();
+        }
+
+        private void OnSenderStoppedDelegate(object sender, TextureShareEventArgs e)
+        {
+            //Debug.Log("SpoutSender.OnSenderStoppedDelegate:"+e.sharingName);
+            if (e.sharingName == sharingName)
+            {
+                _CloseSenderCleanUpData();
+            }
+        }
+
+        private void OnAllSendersStoppedDelegate()
+        {
+            _CloseSenderCleanUpData();
+        }
+
+        private void _CloseSenderCleanUpData()
+        {
+            senderIsCreated = false;
+        }
+    }
+}
